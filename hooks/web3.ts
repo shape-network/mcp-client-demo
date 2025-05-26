c51e977004c1538ba58ab14a6a74513d63e35394
William H <william.hermozo@protonmail.com>
2025-05-26 18:21:24 +0200

    better types

--- hooks/web3.ts
+++ hooks/web3.ts
@@ -1,14 +1,15 @@
 import { alchemy } from "@/lib/clients";
 import { useQuery } from "@tanstack/react-query";
+import { OwnedNftsResponse } from "alchemy-sdk";
 import { Address } from "viem";
 
 /**
  * Example hook to fetch NFTs for a user
  * @param address - User's address to fetch NFTs for
- * @returns react-query's respons object containing data, pending states, errors, etc
+ * @returns react-query's respons object containing OwnedNftsResponse data, pending states, errors, etc
  */
 export function useGetNftForUser(address: Address) {
-  return useQuery({
+  return useQuery<OwnedNftsResponse>({
     queryKey: ["nft", address],
     queryFn: async () => {
       const nft = await alchemy.nft.getNftsForOwner(address, {
