2dbfa8bcc98ed356aebb0d9acdc851dafccc90fb
William H <william.hermozo@protonmail.com>
2025-05-26 19:01:53 +0200

    update theme color

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,6 +9,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    theme_color: "#ffffff",
   };
 }
