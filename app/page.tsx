2dfa30bdb3f68a86ff2835a11e00fdfedc6ced77
William H <william.hermozo@protonmail.com>
2025-05-26 13:00:39 +0200

    ui & content

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { ArrowTopRightIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 items-center justify-center min-h-[75dvh]">
       <div className="flex flex-col items-center gap-2">
-        <h1 className="text-4xl font-bold mt-2 uppercase">dApp Starter</h1>
+        <h1 className="text-4xl tracking-wide font-semibold mt-2 uppercase">
+          dApp Starter
+        </h1>
         <h2>A modern & sleek starter kit for building dApps</h2>
       </div>
 
-      <div className="flex flex-col items-start gap-2">
-        <ul className="flex flex-col items-start gap-2 list-disc">
-          <li>
-            <Link href="/" className="hover:underline font-semibold">
-              Source Code
-            </Link>
-          </li>
-          <li>
-            <Link href="/" className="hover:underline font-semibold">
-              Shape Docs
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="flex items-center gap-16 mt-4">
+        <li>
+          <Link
+            href="https://github.com/shape-network/dapp-starter"
+            className="hover:underline font-semibold flex items-center gap-2"
+          >
+            Source Code <ArrowTopRightIcon />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://docs.shape.network"
+            className="hover:underline font-semibold flex items-center gap-2"
+          >
+            Shape Docs <ArrowTopRightIcon />
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
