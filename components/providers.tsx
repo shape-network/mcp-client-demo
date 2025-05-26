1af570d8b201047a447d0f00e36088c5021fc753
William H <william.hermozo@protonmail.com>
2025-05-26 12:51:53 +0200

    providers

---
+++ components/providers.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { config } from '@/lib/config';
+import { wagmiConfig } from '@/lib/web3';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { ReactNode, Suspense, useEffect } from 'react';
+import { shape, shapeSepolia } from 'viem/chains';
+import { WagmiProvider } from 'wagmi';
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          initialChain={config.chainId === shape.id ? shape : shapeSepolia}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
