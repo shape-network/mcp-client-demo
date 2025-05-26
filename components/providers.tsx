2767c9b620c15428d03edc67f98a54bf6cf9d6ea
William H <william.hermozo@protonmail.com>
2025-05-26 13:48:21 +0200

    add theme

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,6 +4,7 @@ import { config } from "@/lib/config";
 import { wagmiConfig } from "@/lib/web3";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 import { shape, shapeSepolia } from "viem/chains";
 import { WagmiProvider } from "wagmi";
@@ -12,14 +13,21 @@ const queryClient = new QueryClient();
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          initialChain={config.chainId === shape.id ? shape : shapeSepolia}
-        >
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            initialChain={config.chainId === shape.id ? shape : shapeSepolia}
+          >
+            {children}
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ThemeProvider>
   );
 };
