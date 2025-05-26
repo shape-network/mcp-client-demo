6393506142a5436ca9e4f32f6ef4aa38b0bde403
William H <william.hermozo@protonmail.com>
2025-05-26 13:41:06 +0200

    use-balance

---
+++ hooks/use-balance.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { config } from "@/lib/config";
+import { useAccount, useBalance } from "wagmi";
+
+export function useWalletBalance() {
+  const { address, isConnected } = useAccount();
+
+  const {
+    data: balance,
+    isLoading,
+    error,
+  } = useBalance({
+    address,
+    chainId: config.chainId,
+    query: {
+      enabled: isConnected && !!address,
+      refetchInterval: 30000, // Refetch every 30 seconds
+    },
+  });
+
+  return {
+    balance,
+    isLoading,
+    error,
+    isConnected,
+    address,
+  };
+}
