5e476f5583505616f5f40000302c3fb9e5cb8617
William H <william.hermozo@protonmail.com>
2025-08-01 14:27:54 +0200

    url

--- lib/config.ts
+++ lib/config.ts
@@ -2,4 +2,5 @@ export const config = {
   chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
   alchemyKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY as string,
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+  mcpServerUrl: process.env.NEXT_PUBLIC_MCP_SERVER_URL as string,
 } as const;
